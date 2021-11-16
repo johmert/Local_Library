3a511ec4268c90049468d26ac90101ce17eb8903
johmert <johmert@users.noreply.github.com>
2021-11-15 16:51:10 -0800

    completed findAccountById / sortAccountsByLastName

--- public/src/accounts.js
+++ public/src/accounts.js
@@ -1,8 +1,17 @@
-function findAccountById(accounts, id) {}
+function findAccountById(accounts, id) {
+  const result = accounts.find(account => account.id === id);
+  return result;
+}
 
-function sortAccountsByLastName(accounts) {}
+function sortAccountsByLastName(accounts) {
+  const result = accounts.sort((account1, account2) => 
+    account1.name.last.toLowerCase() > account2.name.last.toLowerCase() ? 1 : -1);
+  return result;
+}
 
-function getTotalNumberOfBorrows(account, books) {}
+function getTotalNumberOfBorrows(account, books) {
+  // use reduce method
+}
 
 function getBooksPossessedByAccount(account, books, authors) {}
 
