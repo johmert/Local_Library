15d24925de936fa5274665ebaa2914e97dc94913
johmert <johmert@users.noreply.github.com>
2021-11-17 19:54:11 -0800

    finished book functions

--- public/src/books.js
+++ public/src/books.js
@@ -8,9 +8,27 @@ function findBookById(books, id) {
   return result;
 }
 
-function partitionBooksByBorrowedStatus(books) {}
+function partitionBooksByBorrowedStatus(books) {
+  const checkedOut = books.filter(book => !book.borrows[0].returned);
+  const returned = books.filter(book => book.borrows[0].returned);
+  return [checkedOut, returned];
+}
 
-function getBorrowersForBook(book, accounts) {}
+function getBorrowersForBook(book, accounts) {
+  const borrows = book.borrows;
+  let result = [];
+  borrows.forEach(borrow => {
+    const id = borrow.id;
+    const returned = borrow.returned;
+    const account = accounts.find(acc => acc.id === id);
+    account.returned = returned;
+    result.push(account);
+  });
+  while(result.length > 10) {
+    result.pop();
+  }
+  return result;
+}
 
 module.exports = {
   findAuthorById,
